97b67a0b11e9e5d398fad3b81ba22520a3389222
Emiliano Prandi <emiprandi@gmail.com>
2019-09-21 01:10:27 +0200

    basic project structure

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{lecker}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      lecker: 'Lecker Player'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: tomato;
+}
+</style>
